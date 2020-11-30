5ff957b1dc566bd29c53558ff6c7fde27c123fea
Michiel-VE <r0794174@student.thomasmore.be>
2020-11-30 15:24:26 +0100

    fix unitest v2

--- src/main/java/fact/it/supermarktproject/model/Klant.java
+++ src/main/java/fact/it/supermarktproject/model/Klant.java
@@ -40,7 +40,7 @@ public class Klant extends Persoon {
     }
 
     public String toString() {
-        return "Klant " + super.toString() + " met klantenkaartnr " + '5';
+        return "Klant " + super.toString() + " met klantenkaartnr " + klantenkaartnr;
 
     }
 }
